1f9d48b134e7d3c35a8ec94407340f867f9277a8
Duccio Rocchini <ducciorocchini@gmail.com>
2022-09-21 16:48:39 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -14,4 +14,37 @@ l2011
 
 plot(l2011)
 
+#b1 = blue
+#b2 = green
+#b3 = red
+#b4 = nir
+#b5 = swir
+#b6 = thermal ir (res. 60m)
+#b7 = swir
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011, col=cl)
+
+plot(l2011$B1_sre, col=cl)
+plot(l2011[[1]], col=cl)
+
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011[[1]], col=clb)
+
+par(mfrow=c(2,2)) # the first number is the number of rows in the multiframe, while the second one is the mnumber of columns
+plot(l2011[[1]], col=cl)
+plot(l2011[[2]], col=cl)
+plot(l2011[[3]], col=cl)
+plot(l2011[[4]], col=cl)
+dev.off()
+
+
+
+
+
+
+
+
+
+
 
