9b9a7cda301f20e098c2522d1b4417eeddb735a7
Duccio Rocchini <ducciorocchini@gmail.com>
2022-09-21 17:17:33 +0200

    Update R_code_first.r

--- R_code_first.r
+++ R_code_first.r
@@ -8,6 +8,7 @@ setwd("~/lab/data_book/raster_data/final/") # Linux
 # setwd("/Users/name/lab/") # mac
 
 l2011 <- brick("p224r63_2011.grd")
+l1988 <- brick("p224r63_1988.grd")
 
 l2011
 # crs        : +proj=utm +zone=22 +datum=WGS84 +units=m +no_defs 
@@ -38,6 +39,14 @@ plot(l2011[[3]], col=cl)
 plot(l2011[[4]], col=cl)
 dev.off()
 
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")  # natural colours
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")  # false colours
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")  # false colours
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")  # false colours
+
+par(mfrow=c(2,1)) # the first number is the number of rows in the multiframe, while the second one is the mnumber of columns
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")  # false colours
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")  # false colours
 
 
 
